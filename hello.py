5f576dfdbb975a4eb9763e4373990c2a78ec3da4
Shreevidya <shreevs@cisco.com>
2026-03-23 16:54:54 +0530

    updated

--- hello.py
+++ hello.py
@@ -1,2 +1,26 @@
 print("Hello, Github")
 print('This is my first update')
+# function to check even or odd number
+# function to check even or odd number
+# function to check even or odd number
+
+
+def add(a,b):
+
+
+def add(a, b):
+
+def multiply(a, b):
+
+def multiply(a, b):
+def multiply(a, b):
+
+def add(a, b):
+
+# function to check even or odd number
+def add(a, b):
+# function to check even or odd number    
+# function to check even or odd number 
+
+Print("Hello, PY")  
+
